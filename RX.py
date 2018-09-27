9a33d0ddef321b4e53420041c2bf261181783d9a
Bruno Antônio de Pinho <bruno.antonio.pinho@gmail.com>
2018-09-27 07:41:36 -0300

    ARQ funcionando

--- RX.py
+++ RX.py
@@ -3,9 +3,13 @@ import serial
 import arq	
 
 #ser = serial.Serial('/dev/ttyUSB0',9600)
-ser = serial.Serial('/dev/pts/7',9600)
+ser = serial.Serial('/dev/pts/3',9600)
 frame = enq.Enquadramento(ser)
 data = arq.ARQ(frame, 0, 0)
 msg = data.recebe()
-print(msg)
-#frame.recebe()
+print(msg[1])
+msg = data.recebe()
+print(msg[1])
+msg = data.recebe()
+print(msg[1])
+data.envia(b'No!')
